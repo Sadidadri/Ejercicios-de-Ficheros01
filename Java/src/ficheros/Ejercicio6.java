b682152c061fbcdeeb14a82d1f9e155d9671418d
Sadidadri <36188656+Sadidadri@users.noreply.github.com>
2019-04-02 11:16:24 +0200

    Add files via upload

--- Java/src/ficheros/Ejercicio6.java
+++ Java/src/ficheros/Ejercicio6.java
@@ -5,9 +5,10 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 /**
- * Realiza un programa que diga cuántas ocurrencias de una palabra hay en un
+ * Realiza un programa que diga cuï¿½ntas ocurrencias de una palabra hay en un
  * fichero. Tanto el nombre del fichero como la palabra se deben pasar como
- * argumentos en la línea de comandos.
+ * argumentos en la lï¿½nea de comandos.
+ * 
  * @author d18momoa
  *
  */
@@ -15,25 +16,34 @@ public class Ejercicio6 {
 
   public static void main(String[] args) {
     try {
-      BufferedReader br = new BufferedReader(new FileReader(args[0]+".txt"));
+      // BufferedReader br = new BufferedReader(new FileReader(args[0]+".txt"));
+      BufferedReader br = new BufferedReader(new FileReader("loremIpsum.txt"));
       String linea = "";
-      String palabraDeseada = ""+args[1]; //Palabra la cual queremos buscar en el fichero
-      int coincidencias = 0; //Numero de veces que se encuentra la palabra
-      while(linea != null) {
+      // String palabraDeseada = ""+args[1]; //Palabra la cual queremos buscar en el
+      // fichero
+      String palabraDeseada = "amet"; // Palabra la cual queremos buscar en el fichero
+      int coincidencias = 0; // Numero de veces que se encuentra la palabra
+      while (linea != null) {
         linea = br.readLine();
-        String[] palabras = linea.split(" "); //Separa las palabras separadas por espacios y las almacena en un array de string
-        for(String x : palabras) { //Recorre el array y si coincide con la palabra deseada, aumenta el contador
+        String[] palabras = new String[] { "" }; // Se inicializa para evitar que en el foreach pueda dar un
+                                                 // NullPointerExceptionAlFinal
+        if (linea != null) {
+          palabras = linea.split(" "); // Separa las palabras separadas por espacios y las almacena en un array de
+                                       // string
+        }
+        for (String x : palabras) { // Recorre el array y si coincide con la palabra deseada, aumenta el contador
           if (x.equals(palabraDeseada)) {
             coincidencias++;
           }
         }
       }
-      
-      System.out.println("La palabra "+palabraDeseada+" se ha encontrado "+coincidencias+" veces.");
+
+      System.out.println("La palabra " + palabraDeseada + " se ha encontrado " + coincidencias + " veces.");
       System.out.println("Programa terminado");
-      br.close(); //Cierre de archivo
+      br.close(); // Cierre de archivo
     } catch (Exception e) {
-      System.err.println("Error, no se ha podido encontrar esas coincidencias.");
+      System.err.println(
+          "Error, no se ha podido llevar a cabo el programa por la siguiente excepcion:\n" + e.getLocalizedMessage());
     }
   }
 
